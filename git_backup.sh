cbe3f2c5691dad29fc30f60d674f287d1601de17
root <root@instance-1.us-west4-b.c.fluid-axe-350821.internal>
2022-06-01 14:03:04 +0000

    Wed Jun  1 14:03:04 UTC 2022

---
+++ git_backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## every 4 hours
+## 0 */4 * * * /data/git_backup.sh > /data/git_backup.log
+echo "backup service is running"
+
+cd /data
+
+git config --system core.longpaths true
+
+git status		
+git add /data/
+git status
+git commit -m "$(date)"
+git push origin main 
